3882f960cc0f59176799ea67fc78c09d03b72e8d
Chris Keathley <c@keathley.io>
2021-01-19 08:59:15 -0500

    initial commit

---
+++ lib/sync_dispatch.ex
@@ -0,0 +1,18 @@
+defmodule SyncDispatch do
+  @moduledoc """
+  Documentation for `SyncDispatch`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SyncDispatch.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sync_dispatch_test.exs
@@ -0,0 +1,8 @@
+defmodule SyncDispatchTest do
+  use ExUnit.Case
+  doctest SyncDispatch
+
+  test "greets the world" do
+    assert SyncDispatch.hello() == :world
+  end
+end
