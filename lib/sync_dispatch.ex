f78c969b6a07d8cfac165081004277ef7bad16b2
Chris Keathley <c@keathley.io>
2021-01-20 13:47:05 -0500

    Add docs

--- lib/sync_dispatch.ex
+++ lib/sync_dispatch.ex
@@ -1,18 +1,33 @@
 defmodule SyncDispatch do
-  @moduledoc """
-  SyncDispatch provides an api for event driven handlers. Each event handler
-  is synchronously executed in the calling process. Each function is executed
-  """
+  @moduledoc "README.md"
+             |> File.read!()
+             |> String.split("<!-- MDOC !-->")
+             |> Enum.fetch!(1)
+
   alias SyncDispatch.HandlerTable
 
+  @type handler_function :: (list(atom()), term(), term() -> any())
+
+  @doc """
+  Executes an event and any handlers that are attached to the event name. `data`
+  can be any term().
+  """
+  @spec execute(list(atom()), term()) :: :ok | no_return()
   def execute(event, data) do
     handlers = HandlerTable.handlers_for_event(event)
 
     for {handler, event, config} <- handlers do
       handler.(event, data, config)
     end
+
+    :ok
   end
 
+  @doc """
+  Attaches a function to an event. The provided id is used for idempotence.
+  Different handlers should use unique handler ids.
+  """
+  @spec attach(term(), list(atom()), handler_function(), term()) :: :ok | {:error, %SyncDispatch.AttachError{}}
   def attach(id, event, fun, config \\ nil) do
     HandlerTable.insert(id, event, fun, config)
   end
--- mix.exs
+++ mix.exs
@@ -1,13 +1,19 @@
 defmodule SyncDispatch.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :sync_dispatch,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/keathley/sync_dispatch",
+      docs: docs()
     ]
   end
 
@@ -22,6 +28,29 @@ defmodule SyncDispatch.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.23", only: :dev, runtime: false},
+    ]
+  end
+
+  defp description do
+    """
+    SyncDispatch provides a system for Design by Contract in Elixir.
+    """
+  end
+
+  def package do
+    [
+      name: "sync_dispatch",
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/keathley/sync_dispatch"}
+    ]
+  end
+
+  def docs do
+    [
+      main: "SyncDispatch",
+      source_ref: "v#{@version}",
+      source_url: "https://github.com/keathley/sync_dispatch"
     ]
   end
 end
